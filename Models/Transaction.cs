058681c1e034a1cefecc8969880a9bb7b7d68ed8
Furtuna Adrian <74062824+AdrianFurtuna@users.noreply.github.com>
2022-10-06 10:06:42 +0300

    Initial commit

---
+++ Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expanse_tracker.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+
+        //Category
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
